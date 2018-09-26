145c7604bd1df3dd3652da250540388ce6dcfaca
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-26 22:27:10 +0700

    Move metadata from App::XTermUtils

--- lib/XTerm/Util.pm
+++ lib/XTerm/Util.pm
@@ -14,6 +14,44 @@ our @EXPORT_OK = qw(
                        set_term_bgcolor
                );
 
+our %SPEC;
+
+$SPEC{get_term_bgcolor} = {
+    v => 1.1,
+    summary => 'Get terminal background color',
+    description => <<'_',
+
+Get the terminal's current background color, or undef if unavailable. This uses
+the following xterm control sequence:
+
+    \e]11;?\a
+
+and a compatible terminal will issue back the same sequence but with the
+question mark replaced by the RGB code, e.g.:
+
+    \e]11;rgb:0000/0000/0000\a
+
+I have tested this works on the following terminal software (and version) on
+Linux:
+
+    MATE Terminal (1.18.2)
+    GNOME Terminal (3.18.3)
+    Konsole (16.04.3)
+
+And does not work with the following terminal software (and version) on Linux:
+
+    LXTerminal (0.2.0)
+    rxvt (2.7.10)
+
+A 6-hexdigit RGB value will be returned, e.g.:
+
+    000000
+    310035
+
+_
+    args => {},
+    result_naked => 1,
+};
 sub get_term_bgcolor {
     return undef unless -x "/bin/sh";
 
@@ -49,6 +87,34 @@ echo $result >}.$fname2;
     $rgb;
 }
 
+$SPEC{set_term_bgcolor} = {
+    v => 1.1,
+    summary => 'Set terminal background color',
+    description => <<'_',
+
+Set terminal background color. This prints the following xterm control sequence
+to STDOUT (or STDERR, if ~stderr~ is set to true:
+
+    \e]11;#123456\a
+
+where *123456* is the 6-hexdigit RGB color code.
+
+_
+    args_as => 'array',
+    args => {
+        rgb => {
+            schema => 'color::rgb24*',
+            req => 1,
+            pos => 0,
+        },
+        stderr => {
+            schema => 'true*',
+            pos => 1,
+        },
+
+    },
+    result_naked => 1,
+};
 sub set_term_bgcolor {
     my ($rgb, $stderr) = @_;
     $rgb =~ s/\A#?([0-9A-Fa-f]{6})\z/$1/
@@ -61,6 +127,7 @@ sub set_term_bgcolor {
     } else {
         print $str;
     }
+    return;
 }
 
 1;
@@ -86,59 +153,6 @@ sub set_term_bgcolor {
 =head1 DESCRIPTION
 
 Keywords: xterm, xterm-256color, terminal
-
-
-=head1 FUNCTIONS
-
-=head2 get_term_bgcolor
-
-Usage:
-
- my $rgb = get_term_bgcolor();
-
-Get the terminal's current background color, or undef if unavailable. This uses
-the following xterm control sequence:
-
- \e]11;?\a
-
-and a compatible terminal will issue back the same sequence but with the
-question mark replaced by the RGB code, e.g.:
-
- \e]11;rgb:0000/0000/0000\a
-
-I have tested this works on the following terminal software (and version) on
-Linux:
-
- MATE Terminal (1.18.2)
- GNOME Terminal (3.18.3)
- Konsole (16.04.3)
-
-And does not work with the following terminal software (and version) on Linux:
-
- LXTerminal (0.2.0)
- rxvt (2.7.10)
-
-The function will return a 6-hexdigit RGB value, e.g.:
-
- 000000
- 310035
-
-which you can feed to, e.g.: L</"set_term_bgcolor"> to set background color of
-terminal, or L<Color::ANSI::Util>'s C<ansibg> to produce an appropriate escape
-sequance to set background color of text.
-
-=head2 set_term_bgcolor
-
-Usage:
-
- set_term_bgcolor($rgb [, $stderr ]);
-
-Set terminal background color. This prints the following xterm control sequence
-to STDOUT (or STDERR, if C<$stderr> is set to true):
-
- \e]11;#123456\a
-
-where C<123456> is the 6-hexdigit RGB color code.
 
 
 =head1 SEE ALSO
